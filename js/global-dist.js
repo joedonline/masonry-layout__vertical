bc5c2f1141c4b1f2a3c187ce86e8ca191ac2bd30
joedonline <contact@joedomingo.online>
2019-07-04 18:48:05 -0400

    Animation preset.

--- js/global-dist.js
+++ js/global-dist.js
@@ -1,7 +1,31 @@
-"use strict";
+'use strict';
 
 // SITE-WIDE FUNCTIONS AND VARIABLES
 
+
+/*
+ Animation packages - aniHandler() function
+ @param cdn - CDN link + markup
+ @param loc - Local file ref + markup
+*/
+var cdnCSSPluginRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/plugins/CSSPlugin.min.js"><\/script>');
+var cdnEasePackRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/easing/EasePack.min.js"><\/script>');
+var cdnTweenLineMaxRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js"><\/script>');
+var cdnTweenLiteRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenLite.min.js"><\/script>');
+
+var locCSSPluginRef = document.write('<script type="text/javascript" src="gsap/CSSPlugin.min.js"><\/script>');
+var locEasePackRef = document.write('<script type="text/javascript" src="gsap/EasePack.min.js"><\/script>');
+var locTweenLineMaxRef = document.write('<script type="text/javascript" src="gsap/TweenLite.min.js"><\/script>');
+var locTweenLiteRef = document.write('<script type="text/javascript" src="gsap/TimelineMax.min.js"><\/script>');
+
+var aniHandler = function aniHandler(cdn, loc) {
+  return cdn || loc;
+};
+
+aniHandler(cdnCSSPluginRef, locCSSPluginRef);
+aniHandler(cdnEasePackRef, locEasePackRef);
+aniHandler(cdnTweenLineMaxRef, locTweenLineMaxRef);
+aniHandler(cdnTweenLiteRef, locTweenLiteRef);
 
 /*
  Items fetch function
--- js/global.js
+++ js/global.js
@@ -2,6 +2,29 @@
 
 
 /*
+ Animation packages - aniHandler() function
+ @param cdn - CDN link + markup
+ @param loc - Local file ref + markup
+*/
+const cdnCSSPluginRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/plugins/CSSPlugin.min.js"><\/script>');
+const cdnEasePackRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/easing/EasePack.min.js"><\/script>');
+const cdnTweenLineMaxRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js"><\/script>');
+const cdnTweenLiteRef = document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenLite.min.js"><\/script>');
+
+const locCSSPluginRef = document.write('<script type="text/javascript" src="gsap/CSSPlugin.min.js"><\/script>');
+const locEasePackRef = document.write('<script type="text/javascript" src="gsap/EasePack.min.js"><\/script>');
+const locTweenLineMaxRef = document.write('<script type="text/javascript" src="gsap/TweenLite.min.js"><\/script>');
+const locTweenLiteRef = document.write('<script type="text/javascript" src="gsap/TimelineMax.min.js"><\/script>');
+
+const aniHandler = (cdn, loc) => cdn || loc;
+
+aniHandler(cdnCSSPluginRef, locCSSPluginRef);
+aniHandler(cdnEasePackRef, locEasePackRef);
+aniHandler(cdnTweenLineMaxRef, locTweenLineMaxRef);
+aniHandler(cdnTweenLiteRef, locTweenLiteRef);
+
+
+/*
  Items fetch function
  @param ep - These are API endpoints to fetch data from.
 */
